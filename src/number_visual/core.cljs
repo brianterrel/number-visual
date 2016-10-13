2da4f463c6d691b23af7d3fa6e1fd59cbd4ca216
Brian Terrel <brianterrel@gmail.com>
2016-10-12 23:08:17 -0700

    drawing code moved into a couple somewhat wonky functions. there's an off by one error that i'm correcting for in an awkward way. should be able to add one wrapper and make it do the thing

--- src/number_visual/core.cljs
+++ src/number_visual/core.cljs
@@ -5,13 +5,48 @@
 
 ;; define your app data so that it doesn't get over-written on reload
 
+;(defn make-points
+;  "selects evenly spaced points around a circle"
+;  [number size offset]
+;  (for [i (map inc (range number))]
+;        [:circle {:r size
+;                  :cy (- 50 (* offset (Math/cos (/ (* i (* 2 Math/PI)) number))))
+;                  :cx (+ 50 (* offset (Math/sin (/ (* i (* 2 Math/PI)) number))))}]))
+
 (defn make-points
-  "selects evenly spaced points around a circle"
-  [number]
-  (for [i (map inc (range number))]
-        [:circle {:r 10
-                  :cy (- 50 (* 40 (Math/cos (/ (* i (* 2 Math/PI)) number))))
-                  :cx (+ 50 (* 40 (Math/sin (/ (* i (* 2 Math/PI)) number))))}]))
+  "selects evenly spaced points around a circles from a vector of center points"
+  [centers number offset-radius]
+  (loop [point (first centers)
+         loop-seq (rest centers)
+         acc ()]
+    (if (empty? loop-seq)
+      (concat acc (for [i (map inc (range number))]
+                        {:y (- (:y point) (* offset-radius (Math/cos (/ (* i (* 2 Math/PI)) number))))
+                         :x (+ (:x point) (* offset-radius (Math/sin (/ (* i (* 2 Math/PI)) number))))}))
+      (recur (first loop-seq)
+             (rest loop-seq)
+             (concat acc (for [i (map inc (range number))]
+                        {:y (- (:y point) (* offset-radius (Math/cos (/ (* i (* 2 Math/PI)) number))))
+                         :x (+ (:x point) (* offset-radius (Math/sin (/ (* i (* 2 Math/PI)) number))))}))))))
+
+(make-points [{:x 50 :y 50}] 3 30)
+
+
+(defn draw-circles
+  "returns a list of circle elements from a vector of center points"
+  [centers radius]
+  (loop [point (first centers)
+         loop-seq (rest centers)
+         acc ()]
+    (if (empty? loop-seq)
+      (concat acc (vector [:circle {:r radius
+                                  :cx (:x point)
+                                  :cy (:y point)}]))
+      (recur (first loop-seq)
+             (rest loop-seq)
+             (concat acc (vector [:circle {:r radius
+                                  :cx (:x point)
+                                  :cy (:y point)}]))))))
 
 (defonce app-state (atom {:text "Let's look at a number!"
                           :number 2}))
@@ -23,7 +58,7 @@
     {:view-box "0 0 100 100"
      :width 500
      :height 500}
-    (make-points 7)]])
+    (draw-circles (make-points [{:x 50 :y 50}] 5 30) 10)]])
 
 (reagent/render-component [number-visual]
                           (. js/document (getElementById "app")))
